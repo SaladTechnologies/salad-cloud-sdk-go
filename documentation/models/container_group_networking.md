a3367e03845a092fcf155a92d45c90cab0d1619e
SaladTechnologies <415806+seniorquico@users.noreply.github.com>
2024-10-04 04:24:56 +0000

    liblab SDK update for version v0.9.0-alpha.5

--- documentation/models/container_group_networking.md
+++ documentation/models/container_group_networking.md
@@ -4,9 +4,22 @@ Represents container group networking parameters
 
 **Properties**
 
-| Name     | Type                               | Required | Description |
-| :------- | :--------------------------------- | :------- | :---------- |
-| Protocol | shared.ContainerNetworkingProtocol | ✅       |             |
-| Port     | int64                              | ✅       |             |
-| Auth     | bool                               | ✅       |             |
-| Dns      | string                             | ✅       |             |
+| Name                  | Type                                        | Required | Description |
+| :-------------------- | :------------------------------------------ | :------- | :---------- |
+| Protocol              | shared.ContainerNetworkingProtocol          | ✅       |             |
+| Port                  | int64                                       | ✅       |             |
+| Auth                  | bool                                        | ✅       |             |
+| Dns                   | string                                      | ✅       |             |
+| LoadBalancer          | shared.ContainerGroupNetworkingLoadBalancer | ❌       |             |
+| SingleConnectionLimit | bool                                        | ❌       |             |
+| ClientRequestTimeout  | int64                                       | ❌       |             |
+| ServerResponseTimeout | int64                                       | ❌       |             |
+
+# ContainerGroupNetworkingLoadBalancer
+
+**Properties**
+
+| Name                        | Type   | Required | Description                   |
+| :-------------------------- | :----- | :------- | :---------------------------- |
+| round_robin                 | string | ✅       | "round_robin"                 |
+| least_number_of_connections | string | ✅       | "least_number_of_connections" |
--- pkg/containergroups/create_container_group_networking.go
+++ pkg/containergroups/create_container_group_networking.go
@@ -6,9 +6,13 @@ import (
 
 // Represents container group networking parameters
 type CreateContainerGroupNetworking struct {
-	Protocol *shared.ContainerNetworkingProtocol `json:"protocol,omitempty" required:"true"`
-	Port     *int64                              `json:"port,omitempty" required:"true" min:"1" max:"65535"`
-	Auth     *bool                               `json:"auth,omitempty" required:"true"`
+	Protocol              *shared.ContainerNetworkingProtocol         `json:"protocol,omitempty" required:"true"`
+	Port                  *int64                                      `json:"port,omitempty" required:"true" min:"1" max:"65535"`
+	Auth                  *bool                                       `json:"auth,omitempty" required:"true"`
+	LoadBalancer          *CreateContainerGroupNetworkingLoadBalancer `json:"load_balancer,omitempty"`
+	SingleConnectionLimit *bool                                       `json:"single_connection_limit,omitempty"`
+	ClientRequestTimeout  *int64                                      `json:"client_request_timeout,omitempty" min:"1" max:"100000"`
+	ServerResponseTimeout *int64                                      `json:"server_response_timeout,omitempty" min:"1" max:"100000"`
 }
 
 func (c *CreateContainerGroupNetworking) SetProtocol(protocol shared.ContainerNetworkingProtocol) {
@@ -43,3 +47,54 @@ func (c *CreateContainerGroupNetworking) GetAuth() *bool {
 	}
 	return c.Auth
 }
+
+func (c *CreateContainerGroupNetworking) SetLoadBalancer(loadBalancer CreateContainerGroupNetworkingLoadBalancer) {
+	c.LoadBalancer = &loadBalancer
+}
+
+func (c *CreateContainerGroupNetworking) GetLoadBalancer() *CreateContainerGroupNetworkingLoadBalancer {
+	if c == nil {
+		return nil
+	}
+	return c.LoadBalancer
+}
+
+func (c *CreateContainerGroupNetworking) SetSingleConnectionLimit(singleConnectionLimit bool) {
+	c.SingleConnectionLimit = &singleConnectionLimit
+}
+
+func (c *CreateContainerGroupNetworking) GetSingleConnectionLimit() *bool {
+	if c == nil {
+		return nil
+	}
+	return c.SingleConnectionLimit
+}
+
+func (c *CreateContainerGroupNetworking) SetClientRequestTimeout(clientRequestTimeout int64) {
+	c.ClientRequestTimeout = &clientRequestTimeout
+}
+
+func (c *CreateContainerGroupNetworking) GetClientRequestTimeout() *int64 {
+	if c == nil {
+		return nil
+	}
+	return c.ClientRequestTimeout
+}
+
+func (c *CreateContainerGroupNetworking) SetServerResponseTimeout(serverResponseTimeout int64) {
+	c.ServerResponseTimeout = &serverResponseTimeout
+}
+
+func (c *CreateContainerGroupNetworking) GetServerResponseTimeout() *int64 {
+	if c == nil {
+		return nil
+	}
+	return c.ServerResponseTimeout
+}
+
+type CreateContainerGroupNetworkingLoadBalancer string
+
+const (
+	CREATE_CONTAINER_GROUP_NETWORKING_LOAD_BALANCER_ROUND_ROBIN                 CreateContainerGroupNetworkingLoadBalancer = "round_robin"
+	CREATE_CONTAINER_GROUP_NETWORKING_LOAD_BALANCER_LEAST_NUMBER_OF_CONNECTIONS CreateContainerGroupNetworkingLoadBalancer = "least_number_of_connections"
+)
